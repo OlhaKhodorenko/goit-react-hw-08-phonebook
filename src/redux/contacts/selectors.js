7d35f0c892623be7664811151f08e7e32a4ca87c
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-09-05 13:13:46 +0300

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -3,7 +3,6 @@ import { createSelector } from '@reduxjs/toolkit';
 const getContacts = state => state.contacts.items;
 const getFilter = state => state.contacts.filter;
 const getLoading = state => state.contacts.loading;
-
 const getFilteredContacts = createSelector(
   [getContacts, getFilter],
   (contacts, filter) => {
